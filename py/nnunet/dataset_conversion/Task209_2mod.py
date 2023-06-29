1e818174f6afe9f02d8b4478b3bb1fb5b6498a9a
Mona <lixinqi98@gmail.com>
2023-06-29 16:53:47 +0200

    task209

--- py/nnunet/dataset_conversion/Task209_2mod.py
+++ py/nnunet/dataset_conversion/Task209_2mod.py
@@ -6,6 +6,7 @@ from nnunet.utilities.file_conversions import convert_2d_image_to_nifti
 
 import shutil
 import re
+from pathlib import Path
 
 if __name__ == '__main__':
     """
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     magnitude and phase modalities. Two version of data
     """
 
-    base = '/home/xinqili/dlmri/dlmri/nnUNet/nnUNet_data'
+    base = '/home/mona/projects/DynamicShim/data/nnUNet_raw_data_base/nnUNet_raw_data'
     # this folder should have the training and testing subfolders
 
     # now start the conversion to nnU-Net:
@@ -49,15 +50,17 @@ if __name__ == '__main__':
     assert len(images_mag) == len(labels), 'the number of images is different from labels'
 
     for idx, t in enumerate(images_mag):
-        start_idx = re.search('MID', t).start()
-        unique_name = t[start_idx:start_idx+17] # just the filename with the patient number
+        start_idx = re.search('Cardiac', t).start()
+        #unique_name = t[start_idx:start_idx+17] # just the filename with the patient number
+        unique_name = (Path(t).stem).split("_")[0]
+        print(unique_name)
         input_segmentation_file = os.path.join(label_folder, labels[idx])
         input_mag_file = os.path.join(image_folder, t)
         input_phase_file = os.path.join(image_folder, images_phase[idx])
 
 
-        output_mag_file = join(target_imagesTr, unique_name + t[-12:]) 
-        output_phase_file = join(target_imagesTr, unique_name + images_phase[idx][-12:])
+        output_mag_file = join(target_imagesTr, unique_name + '_0000.nii.gz') 
+        output_phase_file = join(target_imagesTr, unique_name + '_0001.nii.gz')
         output_seg_file = join(target_labelsTr, unique_name + ".nii.gz")
 
         shutil.copy(input_mag_file, output_mag_file)
@@ -67,4 +70,3 @@ if __name__ == '__main__':
     # finally we can call the utility for generating a dataset.json
     generate_dataset_json(join(target_base, 'dataset.json'), target_imagesTr, target_imagesTs, ("mag", "phase"),
                           labels={0: 'background', 1: 'heart'}, dataset_name=task_name, license='Mona')
-
